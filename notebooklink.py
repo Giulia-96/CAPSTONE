2c8c250e3b4f199929863ba7ed843422d680de14
Giulia Largia <Giulia@MacBook-Pro-di-Giulia-4.local>
2019-08-08 00:40:06 +0200

    capstone

---
+++ notebooklink.py
@@ -0,0 +1,1 @@
+print('https://eu-de.dataplatform.cloud.ibm.com/analytics/notebooks/v2/1b24a4f2-55bf-4d84-888b-6f3314f323ab/view?access_token=92c4bc8125704557bad0b6f704882ba9d062184eed23bcc41687572425ff9693')
